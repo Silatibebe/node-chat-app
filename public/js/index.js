e704fe7147ea318401fea0c0830a087513418877
Silatibebe <bereketyoseph2013@gmail.com>
2018-02-21 20:02:23 -0800

    Greet new user and alert others

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,7 @@
 var socket = io();
 socket.on('connect',function(){
     console.log('connected to server!');
+    
 });
 //listen for event emitted by server
 socket.on('disconnect',function(){
--- server/server.js
+++ server/server.js
@@ -17,18 +17,34 @@ socket.on('disconnect',() =>{
 console.log('User was disconnected!');
 });
 
+//emit
+socket.emit('newMessage',{
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createAt: new Date().getTime()
+});
+
+//broadcast  
+socket.broadcast.emit('newMessage',{
+    from: 'Admin',
+    text: 'New user joined...',
+    createAt: new Date().getTime()
+});
+
 //listen for event emitted by client
 socket.on('createMessage',(message) =>{
     console.log('createMessage event emitted from client',message);
-    //newMessage
-    io.emit('newMessage',{
-        from: message.from,
-        text: message.text,
-        createdAt: new Date().getTime()
-    });
+   //io emit 
+   io.emit('newMessage',{
+    from: message.from,
+    text: message.text,
+    createdAt: new Date().getTime()
+});
+
 });
 });
 
+//listen
 server.listen(port,function(){//
     console.log(`server is up on port ${port}`);
 });
