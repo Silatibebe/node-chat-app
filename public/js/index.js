c297dce55566fbe546fad5b941f7f1776019d4b5
Silatibebe <bereketyoseph2013@gmail.com>
2018-02-23 22:03:06 -0800

    Add mustache.js for message templates

--- public/js/index.js
+++ public/js/index.js
@@ -11,21 +11,27 @@ socket.on('disconnect',function(){
 //newMessage
 socket.on('newMessage',function(message){
     var formatedTime = moment(message.createdAt).format('h:mm a');
-    console.log('newMessage',message);
-    var li = jQuery('<li></>');
-    li.text(`${message.from}: ${formatedTime}: ${message.text}`);
-    jQuery('#messages').append(li);
+    var template = jQuery('#message-template').html();
+    var html = Mustache.render(template,{
+        text: message.text,
+        from: message.from,
+        createdAt: formatedTime
+    });
+    jQuery('#messages').append(html);
 });
 
 //newLocationMessage
 socket.on('newLocationMessage',function(message){
     var formatedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    var a = jQuery('<a target="_blank">My current location </a>');
-    li.text(`${message.from}: ${formatedTime} `);
-    a.attr('href',message.url);
-    li.append(a);
-    jQuery('#messages').append(li);
+    var template = jQuery('#location-message-template').html();
+    var html = Mustache.render(template,{
+        from: message.from,
+        url: message.url,
+        createdAt: formatedTime
+       
+    });
+    jQuery('#messages').append(html);
+  
 });
 jQuery('#message-form').on('submit', function(e){
     e.preventDefault();
